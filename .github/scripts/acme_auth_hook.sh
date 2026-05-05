1fbb667233b3eecf17e455537c70bb64d78dc4ce
Markus Hoffrogge <mhoffrogge@gmail.com>
2026-05-05 09:51:30 +0200

    Scripts: Add explicit trigger of pages deploy action

--- .github/scripts/acme_auth_hook.sh
+++ .github/scripts/acme_auth_hook.sh
@@ -53,10 +53,12 @@ truncate -s -1 "$CHALLENGE_FILE"
 # Commit challenge file and push to pages branch
 #
 #GH_PAGES_BRANCH="gh-pages"
-echo "GH_PAGES_BRANCH:  '${GH_PAGES_BRANCH}'"
+echo "GH_PAGES_BRANCH:     '${GH_PAGES_BRANCH}'"
 #NO_CHALLENGE_TAG="__no_challenge"
-echo "NO_CHALLENGE_TAG: '${NO_CHALLENGE_TAG}'"
-echo "GITHUB_REF_NAME:  '${GITHUB_REF_NAME}'"
+echo "NO_CHALLENGE_TAG:    '${NO_CHALLENGE_TAG}'"
+echo "GITHUB_REF_NAME:     '${GITHUB_REF_NAME}'"
+echo "GITHUB_REPO:         '${GITHUB_REPO}'"
+echo "GITHUB_PAGES_ACTION: '${GITHUB_PAGES_ACTION}'"
 if [[ -n $(git status --porcelain) && -n "${GITHUB_REF_NAME}" ]]; then
     git fetch --all
     # TODO - move tagging into main workflow
@@ -66,6 +68,16 @@ if [[ -n $(git status --porcelain) && -n "${GITHUB_REF_NAME}" ]]; then
     #git push origin "${GITHUB_REF_NAME}"
     git push --force-with-lease origin "${GITHUB_REF_NAME}":"${GH_PAGES_BRANCH}"
     echo "Challenge force pushed to ${GH_PAGES_BRANCH}."
+    echo "Triggering '${GITHUB_PAGES_ACTION}' on branch '${GH_PAGES_BRANCH}' of '${GITHUB_REPO}'"
+    curl -L \
+         --fail-with-body \
+         -X POST \
+         -H "Accept: application/vnd.github+json" \
+         -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+         -H "X-GitHub-Api-Version: 2022-11-28" \
+         -d '{"ref":"'"${GH_PAGES_BRANCH}"'"}' \
+         "https://api.github.com/repos/${GITHUB_REPO}/actions/workflows/${GITHUB_PAGES_ACTION}/dispatches"
+
 elif [[ -z "${GITHUB_REF_NAME}" ]]; then
     echo "###### WARNING: GITHUB_REF_NAME not set - no change commited - challenge must be updated manually!"
 fi
